44468bc7ed597b3cece68144629c0fe4e0e4b103
Runcong Zhu <rzhu254@uwo.ca>
2025-04-26 20:25:38 -0400

    Setting up objects

---
+++ Motion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BPTrueskill
+{
+    public class Motion
+    {
+        public string Text { get; set; }
+        public int RoundNumber { get; set; }
+        public string TournamentId { get; set; }
+        public int? ClusterLabel { get; set; }
+
+        public Motion(string text, int roundNumber, string tournamentId)
+        {
+            Text = text;
+            RoundNumber = roundNumber;
+            TournamentId = tournamentId;
+            ClusterLabel = null;
+        }
+    }
+}
---
+++ Tournament.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BPTrueskill
+{
+    public class Tournament
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public DateTime Date { get; set; }
+
+        public List<Motion> Motions { get; set; }
+
+        public Tournament(string name, string url, DateTime date)
+        {
+            Name = name;
+            Url = url;
+            Date = date;
+            Motions = new List<Motion>();
+        }
+
+        public void ScrapeMotions(ITournamentScraper scraper)
+        {
+            var motionTexts = scraper.FetchMotions(Url);
+            for (int i = 0; i < motionTexts.Count; i++)
+            {
+                var motion = new Motion(motionTexts[i], i + 1, Name);
+
+                Motions.Add(motion);
+            }
+        }
+    }
+}
